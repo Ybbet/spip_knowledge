b534654ab53eef06c71bef543408c85d83813215
Teddy Payet <teddy.payet@gmail.com>
2015-10-06 00:09:31 +0200

    Ajout du fichier d’autorisation

---
+++ knowledge_autorisations.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Définit les autorisations du plugin Knowledge.
+ *
+ * @plugin     Knowledge
+ *
+ * @copyright  2015
+ * @author     Teddy Payet
+ * @licence    BY-NC-ND
+ */
+if (!defined('_ECRIRE_INC_VERSION')) {
+    return;
+}
+
+/**
+ * Fonction d'appel pour le pipeline.
+ * @pipeline autoriser */
+function knowledge_autoriser()
+{
+}

49c44a6b9184ae28c95becb9c6d79bf83781de18
Teddy Payet <teddy.payet@gmail.com>
2015-10-12 10:55:51 +0200

    Il faut aussi lister les articles des rubriques de niveau 1. Augmentation des niveaux pour permettre une bonne exécution du script de génération de PDF. On redimensionne les images dans le texte, et on passe tout en jpg pour une intégration plus rapide dans la librairie mPDF.

--- knowledge_options.php
+++ knowledge_options.php
@@ -13,6 +13,7 @@ if (!defined('_ECRIRE_INC_VERSION')) {
     return;
 }
 
-ini_set('xdebug.max_nesting_level', 200);
-ini_set('memory_limit', '128M');
-ini_set('pcre.backtrack_limit', '10000000');
+ini_set('xdebug.max_nesting_level', 300);
+ini_set('memory_limit', '256M');
+ini_set('max_execution_time', '240');
+ini_set('pcre.backtrack_limit', '100000000');

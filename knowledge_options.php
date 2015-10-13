09cbab513ce0495335f02ee30105f6ba4fff0671
Teddy Payet <teddy.payet@gmail.com>
2015-10-13 15:45:35 +0200

    Il ne faut pas oublier de définir la globale

--- knowledge_options.php
+++ knowledge_options.php
@@ -17,3 +17,7 @@ ini_set('xdebug.max_nesting_level', 300);
 ini_set('memory_limit', '256M');
 ini_set('max_execution_time', '240');
 ini_set('pcre.backtrack_limit', '100000000');
+
+if (!isset($GLOBALS['z_blocs'])) {
+    $GLOBALS['z_blocs'] = array('content','aside','breadcrumb','head','head_js','header','footer');
+}
